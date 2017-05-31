c94a6a5c4d2fc0fedcf6b702ba07d5c13899409e
guoshencheng <648772021@qq.com>
2017-05-31 19:19:46 +0800

    dist

--- webpack.config.product.js
+++ webpack.config.product.js
@@ -6,7 +6,8 @@ var webpackConfig = Object.assign({}, require('./webpack.config.js'));
 webpackConfig.devtool = "";
 webpackConfig.output.filename = "[name].[hash].js"
 webpackConfig.plugins = [ 
-  new ExtractTextPlugin("[name].[hash].css")
+  new ExtractTextPlugin("[name].[hash].css"),
+  new webpack.optimize.UglifyJsPlugin()
 ]
 
 module.exports = webpackConfig;
